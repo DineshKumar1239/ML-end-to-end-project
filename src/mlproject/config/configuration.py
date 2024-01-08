3ca81bd8f77fc0256c8eb1932bc9cd6424cee9ea
DineshKumar1239 <dineshkumar2192003@gmail.com>
2024-01-08 13:44:24 +0530

    datavald

--- src/mlproject/config/configuration.py
+++ src/mlproject/config/configuration.py
@@ -1,6 +1,7 @@
 from mlproject.constants import *
 from mlproject.utils.common import read_yaml, create_directories
-from mlproject.entity.config_entity import (DataIngestionConfig)
+from mlproject.entity.config_entity import (DataIngestionConfig,
+                                            DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -30,40 +31,34 @@ class ConfigurationManager:
 
         return data_ingestion_config
     
-import os
-import urllib.request as request
-import zipfile
-from mlproject.logging import logger
-from mlproject.utils.common import get_size
 
 
+class ConfigurationManager:
+    def __init__(
+        self,
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH,
+        schema_filepath = SCHEMA_FILE_PATH):
 
-class DataIngestion:
-    def __init__(self, config: DataIngestionConfig):
-        self.config = config
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+        self.schema = read_yaml(schema_filepath)
 
-
-    
-    def download_file(self):
-        if not os.path.exists(self.config.local_data_file):
-            filename, headers = request.urlretrieve(
-                url = self.config.source_URL,
-                filename = self.config.local_data_file
-            )
-            logger.info(f"{filename} download! with following info: \n{headers}")
-        else:
-            logger.info(f"File already exists of size: {get_size(Path(self.config.local_data_file))}")
-
+        create_directories([self.config.artifacts_root])
 
 
     
-    def extract_zip_file(self):
-        """
-        zip_file_path: str
-        Extracts the zip file into the data directory
-        Function returns None
-        """
-        unzip_path = self.config.unzip_dir
-        os.makedirs(unzip_path, exist_ok=True)
-        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
-            zip_ref.extractall(unzip_path)
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
